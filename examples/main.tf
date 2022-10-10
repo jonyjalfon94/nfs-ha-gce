81c930ac968e907f6b2063a949089dfea9fe9445
Jonathan Jalfon <jonatanj@sela.co.il>
2022-10-10 22:27:53 +0300

    Add changes

--- examples/main.tf
+++ examples/main.tf
@@ -4,12 +4,12 @@ locals {
 }
 
 
-module "nfs_cluster" {
-  source       = "../"
-  project_id   = "playground-s-11-d2896fc3"
-  cluster_name = "demo"
-  region       = "us-central1"
-  subnetwork   = module.gcp_network.subnets_names[0]
-  static_ips   = ["10.0.0.4", "10.0.0.5", "10.0.0.6"]
-  disk_size_gb = 20
-}
+# module "nfs_cluster" {
+#   source       = "../"
+#   project_id   = "playground-s-11-d2896fc3"
+#   cluster_name = "demo"
+#   region       = "us-central1"
+#   subnetwork   = module.gcp_network.subnets_names[0]
+#   static_ips   = ["10.0.0.4", "10.0.0.5", "10.0.0.6"]
+#   disk_size_gb = 20
+# }
