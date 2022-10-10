75a72b01c33e78c1a541ba18dd56b3564474348c
Jonathan Jalfon <jonatanj@sela.co.il>
2022-10-10 22:07:04 +0300

    add changes

--- init_nfs.sh
+++ init_nfs.sh
@@ -2,36 +2,36 @@ apt-get update -y
 
 apt-get install drbd-utils
 
-cat > /etc/drbd.d/nfs01.res <<EOF
-resource nfs-vol01 {
- protocol C;
- meta-disk internal;
- device /dev/drbd0;
- disk   /dev/sda1;
- handlers {
-  split-brain "/usr/lib/drbd/notify-split-brain.sh root";
- }
- net {
-  allow-two-primaries no;
-  after-sb-0pri discard-zero-changes;
-  after-sb-1pri discard-secondary;
-  after-sb-2pri disconnect;
-  rr-conflict disconnect;
- }
- disk {
-  on-io-error detach;
- }
- syncer {
-  verify-alg sha1;
- }
- on nfs01 {
-  address  10.10.70.31:7790;
- }
- on nfs02 {
-  address  10.10.70.32:7790;
- }
-}
-EOF
+# cat > /etc/drbd.d/nfs01.res <<EOF
+# resource nfs-vol01 {
+#  protocol C;
+#  meta-disk internal;
+#  device /dev/drbd0;
+#  disk   /dev/sda1;
+#  handlers {
+#   split-brain "/usr/lib/drbd/notify-split-brain.sh root";
+#  }
+#  net {
+#   allow-two-primaries no;
+#   after-sb-0pri discard-zero-changes;
+#   after-sb-1pri discard-secondary;
+#   after-sb-2pri disconnect;
+#   rr-conflict disconnect;
+#  }
+#  disk {
+#   on-io-error detach;
+#  }
+#  syncer {
+#   verify-alg sha1;
+#  }
+#  on nfs01 {
+#   address  10.10.70.31:7790;
+#  }
+#  on nfs02 {
+#   address  10.10.70.32:7790;
+#  }
+# }
+# EOF
 
 # apt-get install nfs-kernel-server pacemaker crmsh -y
 # mkdir /nfsshare
--- nfs_servers.tf
+++ nfs_servers.tf
@@ -35,7 +35,7 @@ module "nfs_server" {
   subnetwork         = var.subnetwork
   num_instances      = 1
   hostname           = "${var.cluster_name}-nfs-server"
-  instance_template  = module.nfs_instance_template.self_link
+  instance_template  = module.nfs_instance_template[count.index].self_link
   region             = var.region
   static_ips         = [var.static_ips[count.index]]
 }
