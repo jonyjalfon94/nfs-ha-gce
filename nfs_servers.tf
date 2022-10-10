332eba7c4c7f0e82d0aab39b2dd8e1f302791947
Jonathan Jalfon <jonatanj@sela.co.il>
2022-10-10 20:52:58 +0300

    add changes

--- nfs_servers.tf
+++ nfs_servers.tf
@@ -13,13 +13,12 @@ module "nfs_instance_template" {
 }
 
 module "nfs_server" {
-  source              = "terraform-google-modules/vm/google//modules/umig"
-  project_id          = var.project_id
-  subnetwork          = var.subnetwork
-  add_hostname_suffix = false
-  num_instances       = 2
-  hostname            = "${var.cluster_name}-nfs-server"
-  instance_template   = module.nfs_instance_template.self_link
-  region              = var.region
-  static_ips          = [var.static_ips[0], var.static_ips[1]]
+  source            = "terraform-google-modules/vm/google//modules/umig"
+  project_id        = var.project_id
+  subnetwork        = var.subnetwork
+  num_instances     = 2
+  hostname          = "${var.cluster_name}-nfs-server"
+  instance_template = module.nfs_instance_template.self_link
+  region            = var.region
+  static_ips        = [var.static_ips[0], var.static_ips[1]]
 }
