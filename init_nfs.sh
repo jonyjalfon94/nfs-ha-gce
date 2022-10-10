4a7efb8393e55ec91c80228b297e35e6a3315388
Jonathan Jalfon <jonatanj@sela.co.il>
2022-10-10 18:54:55 +0300

    change init script

--- init_nfs.sh
+++ init_nfs.sh
@@ -3,5 +3,5 @@ apt-get install nfs-kernel-server -y
 mkdir /nfsshare
 chown nobody:nogroup /nfsshare
 chmod 755 /nfsshare
-echo "/nfsshare    192.168.0.0/18(rw,sync,no_subtree_check)" >> /etc/exports
+echo "/nfsshare    10.0.0.0/17(rw,sync,no_subtree_check)" >> /etc/exports
 systemctl restart nfs-server
